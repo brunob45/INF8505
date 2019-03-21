0c7280983bcb11893af339c9212d657aa93780c0
8505_12 <8505_12@vlsi313.vlsi.polymtl.ca>
2019-03-21 18:19:31 -0400

    continue rsa en c

--- projet/rsa/main.c
+++ projet/rsa/main.c
@@ -1,27 +1,53 @@
 #include <stdio.h>
 
+#define byte unsigned char
+
 // #define ARRAY_SIZE 1024/sizeof(unsigned char)
 #define ARRAY_SIZE 3
-#define ARRAY_TYPE unsigned char*
+#define ARRAY_TYPE byte*
 
 // Inputs
-unsigned char N[ARRAY_SIZE] = {0,0,187};
-unsigned char M[ARRAY_SIZE] = {0,0,72};
-unsigned char E[ARRAY_SIZE] = {0,0,107};
+unsigned int n;
+//byte N[ARRAY_SIZE] = {0,0,12};
+byte M[ARRAY_SIZE] = {0,0,72};
+byte E[ARRAY_SIZE] = {0,0,107};
 
 // Temporary
-unsigned char Mp[ARRAY_SIZE];
+byte Mp[ARRAY_SIZE];
 
 // Constants
-unsigned char ONE[ARRAY_SIZE];
-unsigned char C[ARRAY_SIZE];
+byte ONE[ARRAY_SIZE];
+byte C[ARRAY_SIZE];
 
 // Output
-unsigned char R[ARRAY_SIZE];
+byte R[ARRAY_SIZE];
 
-// unsigned char A[ARRAY_SIZE];
-// unsigned char B[ARRAY_SIZE];
-// unsigned char Z[ARRAY_SIZE];
+byte N[ARRAY_SIZE] = {0,0,12};
+byte A[ARRAY_SIZE] = {0,0,7};
+byte B[ARRAY_SIZE] = {0,0,5};
+
+// byte Z[ARRAY_SIZE];
+
+
+void add_array(ARRAY_TYPE in1, ARRAY_TYPE in2, ARRAY_TYPE out);
+void sub_array(ARRAY_TYPE in1, ARRAY_TYPE in2, ARRAY_TYPE out);
+void div2_array(ARRAY_TYPE in, ARRAY_TYPE out);
+void set_array(ARRAY_TYPE inout, unsigned int value);
+byte check_bit_array(ARRAY_TYPE in, unsigned int bit);
+byte greater_array(ARRAY_TYPE in1, ARRAY_TYPE in2);
+void print_array(ARRAY_TYPE in);
+void set_bit_array(ARRAY_TYPE inout, unsigned int bit);
+
+unsigned int get_n(ARRAY_TYPE in)
+{
+  int n = ARRAY_SIZE*8 - 1;
+  while(n >= 0 && !check_bit_array(in, n))
+  {
+    n--;
+  }
+  return n+1;
+}
+
 
 void add_array(ARRAY_TYPE in1, ARRAY_TYPE in2, ARRAY_TYPE out)
 {
@@ -73,12 +99,12 @@ void set_array(ARRAY_TYPE inout, unsigned int value)
     }
 }
 
-char check_bit_array(ARRAY_TYPE in, unsigned int bit)
+byte check_bit_array(ARRAY_TYPE in, unsigned int bit)
 {
     return !!(in[ARRAY_SIZE-1-bit/8] & 1<<(bit%8));
 }
 
-char greater_array(ARRAY_TYPE in1, ARRAY_TYPE in2)
+byte greater_array(ARRAY_TYPE in1, ARRAY_TYPE in2)
 {
     for(int i = ARRAY_SIZE-1; i >= 0; i--)
     {
@@ -94,9 +120,9 @@ void print_array(ARRAY_TYPE in)
 {
     for(int i = 0; i < ARRAY_SIZE; i++)
     {
-        printf("%x ", in[i]);
+        printf("%x\t", in[i]);
     }
-    printf("%u\n", check_bit_array(in, 8));
+    printf("\n");
 }
 
 void set_bit_array(ARRAY_TYPE inout, unsigned int bit)
@@ -109,19 +135,21 @@ int MMM(ARRAY_TYPE _A, ARRAY_TYPE _B, ARRAY_TYPE _R)
 {
     set_array(_R, 0);
 
-    for(int i = 0; i < ARRAY_SIZE*8; i++)
+    for(int i = 0; i < n; i++)
     {
+      print_array(_R);
         if(check_bit_array(_A, i))
             add_array(_R, _B, _R);
-
+      print_array(_R);
         if(check_bit_array(_R, 0))
             add_array(_R, N, _R);
-
+      print_array(_R);
         div2_array(_R, _R);
     }
-
+    print_array(_R);
     if(greater_array(_R, N))
         sub_array(_R, N, _R);
+    print_array(_R);
 }
 
 void MME(ARRAY_TYPE _M, ARRAY_TYPE _e, ARRAY_TYPE _C, ARRAY_TYPE _R)
@@ -141,12 +169,20 @@ void MME(ARRAY_TYPE _M, ARRAY_TYPE _e, ARRAY_TYPE _C, ARRAY_TYPE _R)
 
 int main(int argc, char** argv)
 {
-    set_array(ONE, 2);
-    
-    set_array(C, 0);
-    set_bit_array(C, 2*(ARRAY_SIZE+2));
+  n = get_n(N);
+  printf("n:%d\n", n);
+  set_array(ONE, 1);
+  
+  set_array(C, 0);
+  set_bit_array(C, 2*(ARRAY_SIZE+2));
 
-    MME(M, E, C, R);
-    print_array(R);
+  MMM(A, B, R);
+  print_array(A);
+  print_array(B);
+  print_array(R);
+
+  return 0;
+  MME(M, E, C, R);
+  print_array(R);
 
 }
