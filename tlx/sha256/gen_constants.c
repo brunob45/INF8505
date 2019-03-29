6f1ed55b03358fbd758f7f170087e6e4d94409fc
Bruno Bousquet <b.b45@hotmail.com>
2019-03-29 16:55:20 -0400

    add program to generate constants

--- tlx/sha256/gen_constants.c
+++ tlx/sha256/gen_constants.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
 #define ARRAY_SIZE (1024/8+1)
-#define ARRAY_TYPE unsigned char*
 
 #include "ops.h"
-#include "keys/keys.h"
 
-byte N[ARRAY_SIZE], R[ARRAY_SIZE], A[ARRAY_SIZE], B[ARRAY_SIZE], Z[ARRAY_SIZE], C[ARRAY_SIZE], ONE[ARRAY_SIZE];
+byte N[ARRAY_SIZE], R[ARRAY_SIZE], C[ARRAY_SIZE], ONE[ARRAY_SIZE];
 
-uint set_constants(ARRAY_TYPE mod, ARRAY_TYPE R, ARRAY_TYPE C)
+uint set_constants()
 {
-    uint n = get_n(mod)+2;
+    uint n = get_n(modulus)+2;
     printf("unsigned int n=%d;\n", n);
     array_bit_set(R, n);
-    array_modulus(R, mod, C);
-    array_mulmod(C, C, mod, C);
+    array_modulus(R, C);
+    array_mulmod(C, C, C);
+    array_set(ONE, 1);
 
     printf("unsigned char modulus[] = {\n");
-    array_print(mod);
+    array_print(modulus);
     printf("};\n");
 
     printf("unsigned char C [] = {\n");
     array_print(C);
+    printf("};\n");
+
+    printf("unsigned char ONE [] = {\n");
+    array_print(ONE);
     printf("};\n");
 
     return n;
@@ -30,7 +33,7 @@ uint set_constants(ARRAY_TYPE mod, ARRAY_TYPE R, ARRAY_TYPE C)
 int main()
 {
     uint n;
-    n = set_constants(modulus, R, C);
+    n = set_constants();
     printf("unsigned char publicExponent[] = {\n");
     array_print(publicExponent);
     printf("};\n");
